cb52c68747c534b45ec159a205e02459ad675d30
mateusVedoy <mateusvedoy@outlook.com>
2024-03-13 16:31:02 -0300

    impl messages management into server

--- server/go-server/src/message/Server.go
+++ server/go-server/src/message/Server.go
@@ -2,10 +2,15 @@ package message
 
 import (
 	"context"
+	"errors"
 	"log"
+	"reflect"
 
 	"github.com/golesson/go-grpc-server/src/protobuf"
+	"github.com/google/uuid"
 )
+
+var messages []*protobuf.Message
 
 type Server struct {
 	protobuf.UnimplementedMessageServiceServer
@@ -16,6 +21,15 @@ func (S *Server) mustEmbedUnimplementedMessageServiceServer() {}
 func (S *Server) Create(ctx context.Context, message *protobuf.CreateMessageRequest) (*protobuf.Response, error) {
 
 	log.Printf("Received message from client: %s", message.Content)
+
+	msg := &protobuf.Message{
+		Identifier: uuid.NewString(),
+		Content:    message.Content,
+		Read:       message.Read,
+	}
+
+	messages = append(messages, msg)
+
 	return &protobuf.Response{
 		StatusCode: 201,
 		Message:    "Ok",
@@ -26,28 +40,40 @@ func (S *Server) Read(ctx context.Context, isRead *protobuf.UnreadMessageRequest
 
 	log.Print("Received request to send unread messages")
 
-	message_one := &protobuf.Message{
-		Identifier: "one",
-		Content:    "Hy from message one",
+	var unread []*protobuf.Message
+
+	for _, msg := range messages {
+		if msg.Read == isRead.Read {
+			unread = append(unread, msg)
+		}
 	}
 
-	message_two := &protobuf.Message{
-		Identifier: "two",
-		Content:    "Hy from message two",
+	if unread == nil {
+		return nil, errors.New("there's no unread messages")
 	}
-
-	var msgs []*protobuf.Message
-
-	msgs = append(msgs, message_one, message_two)
 
 	return &protobuf.Messages{
-		Messages: msgs,
+		Messages: unread,
 	}, nil
 }
 
 func (S *Server) MarkAsRead(ctx context.Context, messageId *protobuf.MessageId) (*protobuf.Response, error) {
 
 	log.Printf("Message %s marked as read.", messageId)
+
+	var updatedMessages []*protobuf.Message
+
+	for _, msg := range messages {
+		if msg.Identifier != messageId.Identifier {
+			updatedMessages = append(updatedMessages, msg)
+		}
+	}
+
+	if reflect.DeepEqual(messages, updatedMessages) {
+		return nil, errors.New("there's no message for given identifier")
+	}
+
+	messages = updatedMessages
 
 	return &protobuf.Response{
 		StatusCode: 200,
